f2cdb804c3d2978497bbe85b6ad633c85ffb4c1f
SpencerNinja <62817123+SpencerNinja@users.noreply.github.com>
2022-01-19 14:05:55 -0700

    finished video 3 shapes

---
+++ SwiftfulThinking-Bootcamp/SwiftfulThinking-Bootcamp/ShapesBootcamp.swift
@@ -0,0 +1,33 @@
+//
+//  ShapesBootcamp.swift
+//  SwiftfulThinking-Bootcamp
+//
+//  Created by Spencer Hurd on 1/19/22.
+//
+
+import SwiftUI
+
+struct ShapesBootcamp: View {
+    var body: some View {
+//        Circle()
+//        Ellipse()
+//        Capsule(style: .circular)
+//        Rectangle()
+        RoundedRectangle(cornerRadius: 10)
+//            .fill(Color.green)
+//            .foregroundColor(.pink)
+//            .stroke(Color.red)
+//            .stroke(Color.blue, lineWidth: 30)
+//            .stroke(Color.orange, style: StrokeStyle(lineWidth: 30, lineCap: .butt, dash: [10]))
+//            .stroke(Color.orange, style: StrokeStyle(lineWidth: 30, lineCap: .round, dash: [30]))
+            .trim(from: 0.4, to: 1.0)
+//            .stroke(Color.purple, lineWidth: 50)
+            .frame(width: 300, height: 200)
+    }
+}
+
+struct ShapesBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        ShapesBootcamp()
+    }
+}
