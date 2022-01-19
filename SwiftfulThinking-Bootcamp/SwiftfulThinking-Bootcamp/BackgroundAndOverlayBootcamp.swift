a0474221acee5ee56cda8f096587ab5c3b72f612
SpencerNinja <62817123+SpencerNinja@users.noreply.github.com>
2022-01-19 16:39:59 -0700

    video 9 9:28

---
+++ SwiftfulThinking-Bootcamp/SwiftfulThinking-Bootcamp/BackgroundAndOverlayBootcamp.swift
@@ -0,0 +1,60 @@
+//
+//  BackgroundAndOverlayBootcamp.swift
+//  SwiftfulThinking-Bootcamp
+//
+//  Created by Spencer Hurd on 1/19/22.
+//
+
+import SwiftUI
+
+struct BackgroundAndOverlayBootcamp: View {
+    var body: some View {
+        
+//        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+//            .background(
+////                Color.red
+////                LinearGradient(gradient: Gradient(colors: [Color.red, Color.blue]), startPoint: .leading, endPoint: .trailing)
+//                Circle()
+//                    .fill(LinearGradient(gradient: Gradient(colors: [Color.red, Color.blue]), startPoint: .leading, endPoint: .trailing))
+//                    .frame(width: 100, height: 100, alignment: .center)
+//            )
+//            .background(
+//                Circle()
+//                    .fill(LinearGradient(gradient: Gradient(colors: [Color.blue, Color.red]), startPoint: .leading, endPoint: .trailing))
+//                    .frame(width: 120, height: 120, alignment: .center)
+//            )
+        
+//        Circle()
+//            .fill(Color.pink)
+//            .frame(width: 100, height: 100)
+//            .overlay(
+//                Text("1")
+//                    .font(.largeTitle)
+//                    .foregroundColor(.white)
+//            )
+//            .background(
+//                Circle()
+//                    .fill(Color.purple)
+//                    .frame(width: 110, height: 110)
+//            )
+        
+        Rectangle()
+            .frame(width: 100, height: 100)
+            .overlay(
+                Rectangle()
+                    .fill(Color.blue)
+                    .frame(width: 50, height: 50, alignment: .center)
+            )
+            .background(
+                Rectangle()
+                    .fill(Color.red)
+                    .frame(width: 150, height: 150, alignment: .center)
+            )
+    }
+}
+
+struct BackgroundAndOverlayBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        BackgroundAndOverlayBootcamp()
+    }
+}
