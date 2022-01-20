91d59ce866cd3ee2821cdb199625f62c37ba9ec4
SpencerNinja <62817123+SpencerNinja@users.noreply.github.com>
2022-01-20 13:35:16 -0700

    finished video 9 on Backgrounds

--- SwiftfulThinking-Bootcamp/SwiftfulThinking-Bootcamp/BackgroundAndOverlayBootcamp.swift
+++ SwiftfulThinking-Bootcamp/SwiftfulThinking-Bootcamp/BackgroundAndOverlayBootcamp.swift
@@ -38,18 +38,59 @@ struct BackgroundAndOverlayBootcamp: View {
 //                    .frame(width: 110, height: 110)
 //            )
         
-        Rectangle()
-            .frame(width: 100, height: 100)
-            .overlay(
-                Rectangle()
-                    .fill(Color.blue)
-                    .frame(width: 50, height: 50, alignment: .center)
-            )
+//        Rectangle()
+//            .frame(width: 100, height: 100)
+//            .overlay(
+//                Rectangle()
+//                    .fill(Color.blue)
+//                    .frame(width: 50, height: 50),
+//                    alignment: .topLeading
+//            )
+//            .background(
+//                Rectangle()
+//                    .fill(Color.red)
+//                    .frame(width: 150, height: 150),
+//                    alignment: .bottomTrailing
+//            )
+        
+        Image(systemName: "heart.fill")
+            .font(.system(size: 40))
+            .foregroundColor(.white)
             .background(
-                Rectangle()
-                    .fill(Color.red)
-                    .frame(width: 150, height: 150, alignment: .center)
+                Circle()
+                    .fill(
+                        LinearGradient(
+                            gradient: Gradient(colors: [Color(#colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)), Color(#colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1))]),
+                            startPoint: .topLeading,
+                            endPoint: .bottomTrailing
+                        )
+                    )
+                    .frame(width: 100, height: 100)
+                    .shadow(
+                        color: Color(#colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 0.5)),
+                        radius: 10,
+                        x: 0.0,
+                        y: 10
+                    )
+                    .overlay(
+                        Circle()
+                            .fill(Color.blue)
+                            .frame(width: 35, height: 35)
+                            .overlay(
+                                Text("5")
+                                    .font(.headline)
+                                    .foregroundColor(.white)
+                            )
+                            .shadow(
+                                color: Color(#colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 0.5)),
+                                radius: 10,
+                                x: 5,
+                                y: 5
+                            )
+                        , alignment: .bottomTrailing
+                    )
             )
+        
     }
 }
 
