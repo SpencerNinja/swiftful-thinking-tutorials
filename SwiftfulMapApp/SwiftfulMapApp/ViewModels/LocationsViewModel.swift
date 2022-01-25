d1816bfc80b8bedd51a8e1d3399ec2bfdd5a07af
SpencerNinja <62817123+SpencerNinja@users.noreply.github.com>
2022-01-25 14:03:25 -0700

    finished Map App video 4 adding Map Location to VM

--- SwiftfulMapApp/SwiftfulMapApp/ViewModels/LocationsViewModel.swift
+++ SwiftfulMapApp/SwiftfulMapApp/ViewModels/LocationsViewModel.swift
@@ -6,14 +6,39 @@
 //
 
 import Foundation
+import MapKit
+import SwiftUI
 
 class LocationsViewModel: ObservableObject {
     
+    // All loaded locations
     @Published var locations: [Location]
+    
+    // Current location on map
+    @Published var mapLocation: Location {
+        didSet {
+            updateMapRegion(location: mapLocation)
+        }
+    }
+    
+    @Published var mapRegion: MKCoordinateRegion = MKCoordinateRegion()
+    let mapSpan = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
     
     init() {
         let locations = LocationsDataService.locations
         self.locations = locations
+        // We can explicitly unwrap in this instance because we have data hard-coded in the LocationsDataService locations array.
+        self.mapLocation = locations.first!
+        self.updateMapRegion(location: locations.first!)
+    }
+    
+    private func updateMapRegion(location: Location) {
+        withAnimation(.easeInOut) {
+            mapRegion = MKCoordinateRegion(
+                center: location.coordinates,
+                span: mapSpan
+            )
+        }
     }
     
 }
