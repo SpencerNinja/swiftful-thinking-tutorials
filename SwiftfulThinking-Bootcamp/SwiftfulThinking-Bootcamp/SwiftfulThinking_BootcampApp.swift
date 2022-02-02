efbffcf69b62f25da7bc5b8f11a59d5063dfc006
SpencerNinja <62817123+SpencerNinja@users.noreply.github.com>
2022-02-02 14:41:35 -0700

    finished Bootcamp video 35 on TextField

--- SwiftfulThinking-Bootcamp/SwiftfulThinking-Bootcamp/SwiftfulThinking_BootcampApp.swift
+++ SwiftfulThinking-Bootcamp/SwiftfulThinking-Bootcamp/SwiftfulThinking_BootcampApp.swift
@@ -11,7 +11,8 @@ import SwiftUI
 struct SwiftfulThinking_BootcampApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+//            ContentView()
+            TextfieldBootcamp()
         }
     }
 }
