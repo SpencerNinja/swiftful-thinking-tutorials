e48ca2fe71467041956f17fa4c50e7eac7d90f04
SpencerNinja <62817123+SpencerNinja@users.noreply.github.com>
2022-02-02 15:00:39 -0700

    finished Bootcamp video 36 on TextEditor

---
+++ SwiftfulThinking-Bootcamp/SwiftfulThinking-Bootcamp/TextEditorBootcamp.swift
@@ -0,0 +1,49 @@
+//
+//  TextEditorBootcamp.swift
+//  SwiftfulThinking-Bootcamp
+//
+//  Created by Spencer Hurd on 2/2/22.
+//
+
+import SwiftUI
+
+struct TextEditorBootcamp: View {
+    
+    @State var textEditorText: String = "This is the starting text"
+    @State var savedText: String = ""
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                TextEditor(text: $textEditorText)
+                    .frame(height: 250)
+                    //.foregroundColor(.red)
+                    // Change background color of text box
+                    .colorMultiply(.gray.opacity(0.5))
+                    .cornerRadius(10)
+                Button(action: {
+                    savedText = textEditorText
+                }, label: {
+                    Text("Save".uppercased())
+                        .font(.headline)
+                        .foregroundColor(.white)
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                        .background(Color.blue)
+                        .cornerRadius(10)
+                })
+                Text(savedText)
+                Spacer()
+            }
+            .padding()
+            //.background(Color.green)
+            .navigationTitle("TextEditor Bootcamp")
+        }
+    }
+}
+
+struct TextEditorBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        TextEditorBootcamp()
+    }
+}
