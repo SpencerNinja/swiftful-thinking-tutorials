b0ac94c26ac46a17b3b1001ed881e68773c2a8da
SpencerNinja <spencer.ninja@gmail.com>
2022-02-10 17:48:00 -0700

    finished Bootcamp video 58 on List Swipe Actions

---
+++ SwiftfulThinking-Bootcamp/SwiftfulThinking-Bootcamp/ListSwipeActionsBootcamp.swift
@@ -0,0 +1,63 @@
+//
+//  ListSwipeActionsBootcamp.swift
+//  SwiftfulThinking-Bootcamp
+//
+//  Created by Spencer Hurd on 2/10/22.
+//
+
+import SwiftUI
+
+struct ListSwipeActionsBootcamp: View {
+    
+    @State var fruits: [String] = [
+        "apple", "orange", "banana", "peach"
+    ]
+    
+    var body: some View {
+        List {
+            ForEach(fruits, id: \.self) {
+                Text($0.capitalized)
+                    .swipeActions(edge: .trailing, allowsFullSwipe: false) {
+                        Button("Archive") {
+                            
+                        }
+                        .tint(.green)
+                        Button("Save") {
+                            
+                        }
+                        .tint(.blue)
+                        Button("Junk") {
+                            
+                        }
+                        .tint(.black)
+                    }
+                    .swipeActions(edge: .leading, allowsFullSwipe: true) {
+                        Button("Share") {
+                            
+                        }
+                        .tint(.yellow)
+                    }
+            }
+//            .onDelete(perform: delete)
+        }
+    }
+    
+    func delete(indexSet: IndexSet) {
+        
+    }
+}
+
+struct ListSwipeActionsBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        ListSwipeActionsBootcamp()
+    }
+}
+
+//// THIS METHOD
+//ForEach(fruits, id: \.self) {
+//    Text($0.capitalized)
+//}
+//// IS THE SAME AS THIS
+//ForEach(fruits, id: \.self) { fruit in
+//    Text(fruit.capitalized)
+//}
