4553eb8fa870b4b4d0f6a946ed58e6cb3d177b34
SpencerNinja <62817123+SpencerNinja@users.noreply.github.com>
2022-02-14 15:23:35 -0700

    finished onLongPressGesture video

---
+++ IntermediateBootCamp/IntermediateBootCamp/IntermediateBootCampApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntermediateBootCampApp.swift
+//  IntermediateBootCamp
+//
+//  Created by Spencer Hurd on 2/14/22.
+//
+
+import SwiftUI
+
+@main
+struct IntermediateBootCampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
