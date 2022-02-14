ce11f15d1ea3aca5b79af95fd632f2e518a3d1fd
SpencerNinja <62817123+SpencerNinja@users.noreply.github.com>
2022-02-14 15:43:45 -0700

    finished MagnificationGesture video

--- IntermediateBootCamp/IntermediateBootCamp/IntermediateBootCampApp.swift
+++ IntermediateBootCamp/IntermediateBootCamp/IntermediateBootCampApp.swift
@@ -11,7 +11,8 @@ import SwiftUI
 struct IntermediateBootCampApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+//            ContentView()
+            MagnificationGestureBootcamp()
         }
     }
 }
