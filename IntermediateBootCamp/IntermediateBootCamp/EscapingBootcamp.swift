286c72f83d296afa6a6d8f73acaaeaf45e531cd1
SpencerNinja <62817123+SpencerNinja@users.noreply.github.com>
2022-02-23 16:25:41 -0700

    finished video 20 on escaping closures

---
+++ IntermediateBootCamp/IntermediateBootCamp/EscapingBootcamp.swift
@@ -0,0 +1,86 @@
+//
+//  EscapingBootcamp.swift
+//  IntermediateBootCamp
+//
+//  Created by Spencer Hurd on 2/23/22.
+//
+
+import SwiftUI
+
+class EscapingViewModel: ObservableObject {
+    
+    @Published var text: String = "Hello"
+    
+    func getData() {
+//        downloadData3 { [weak self] (returnedData) in
+//            self?.text = returnedData
+//        }
+        
+//        downloadData4 { [weak self] (returnedResult) in
+//            self?.text = returnedResult.data
+//        }
+        
+        downloadData5 { [weak self] (returnedResult) in
+            self?.text = returnedResult.data
+        }
+    }
+    
+    func downloadData() -> String {
+        return "New data!"
+    }
+    
+    // -> Void
+    // IS THE SAME AS
+    // -> ()
+    func downloadData2(completionHandler: (_ data: String) -> Void) {
+        completionHandler("New data!")
+    }
+    
+    func downloadData3(completionHandler: @escaping (_ data: String) -> ()) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+            completionHandler("New data!")
+        }
+    }
+    
+    func downloadData4(completionHandler: @escaping (DownloadResult) -> ()) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+            let result = DownloadResult(data: "New data!")
+            completionHandler(result)
+        }
+    }
+    
+    func downloadData5(completionHandler: @escaping DownloadCompletion) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+            let result = DownloadResult(data: "New data!")
+            completionHandler(result)
+        }
+    }
+    
+}
+
+struct DownloadResult {
+    let data: String
+}
+
+typealias DownloadCompletion = (DownloadResult) -> ()
+
+struct EscapingBootcamp: View {
+    
+    @StateObject var vm = EscapingViewModel()
+    
+    var body: some View {
+        Text(vm.text)
+            .font(.largeTitle)
+            .fontWeight(.semibold)
+            .foregroundColor(.blue)
+            .onTapGesture {
+                vm.getData()
+            }
+    }
+}
+
+struct EscapingBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        EscapingBootcamp()
+    }
+}
