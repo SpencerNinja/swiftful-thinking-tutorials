fb9f99a4e4e4766ee7dcab2000914ee2bfbf5e87
SpencerNinja <62817123+SpencerNinja@users.noreply.github.com>
2022-03-01 13:29:48 -0700

    finished video 25 on Publishers and Subscribers

--- IntermediateBootCamp/IntermediateBootCamp/SubscriberBootcamp.swift
+++ IntermediateBootCamp/IntermediateBootCamp/SubscriberBootcamp.swift
@@ -16,20 +16,27 @@ class SubscriberViewModel: ObservableObject {
     @Published var textFieldText: String = ""
     @Published var textIsValid: Bool = false
     
+    @Published var showButton: Bool = false
+    
     init() {
         setUpTimer()
         addTextFieldSubscriber()
+        addButtonSubscriber()
     }
     
     func addTextFieldSubscriber() {
         $textFieldText
+            .debounce(for: .seconds(0.5), scheduler: DispatchQueue.main)
             .map { (text) -> Bool in
                 if text.count > 3 {
                     return true
                 }
                 return false
             }
-            .assign(to: \.textIsValid, on: self)
+//            .assign(to: \.textIsValid, on: self)
+            .sink(receiveValue: { [weak self] (isValid) in
+                self?.textIsValid = isValid
+            })
             .store(in: &cancellables)
     }
     
@@ -40,6 +47,20 @@ class SubscriberViewModel: ObservableObject {
             .sink { [weak self] _ in
                 guard let self = self else { return }
                 self.count += 1
+            }
+            .store(in: &cancellables)
+    }
+    
+    func addButtonSubscriber() {
+        $textIsValid
+            .combineLatest($count)
+            .sink { [weak self] (isValid, count) in
+                guard let self = self else { return }
+                if isValid && count >= 10 {
+                    self.showButton = true
+                } else {
+                    self.showButton = false
+                }
             }
             .store(in: &cancellables)
     }
@@ -55,7 +76,7 @@ struct SubscriberBootcamp: View {
             Text("\(vm.count)")
                 .font(.largeTitle)
             
-            Text(vm.textIsValid.description)
+//            Text(vm.textIsValid.description)
             
             TextField("Type something here...", text: $vm.textFieldText)
                 .padding(.leading)
@@ -63,6 +84,33 @@ struct SubscriberBootcamp: View {
                 .font(.headline)
                 .background(Color.gray.opacity(0.1))
                 .cornerRadius(10)
+                .overlay(
+                    ZStack {
+                        Image(systemName: "xmark")
+                            .foregroundColor(.red)
+                            .opacity(
+                                vm.textFieldText.count < 1 ? 0.0 :
+                                vm.textIsValid ? 0.0 : 1.0)
+                        Image(systemName: "checkmark")
+                            .foregroundColor(.green)
+                            .opacity(vm.textIsValid ? 1.0 : 0.0)
+                    }
+                        .font(.title)
+                        .padding(.trailing)
+                    , alignment: .trailing
+                )
+            
+            Button(action: {}, label: {
+                Text("Submit".uppercased())
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .frame(height: 55)
+                    .frame(maxWidth: .infinity)
+                    .background(Color.blue)
+                    .cornerRadius(10)
+                    .opacity(vm.showButton ? 1.0 : 0.5)
+            })
+                .disabled(!vm.showButton)
         }
         .padding()
     }
